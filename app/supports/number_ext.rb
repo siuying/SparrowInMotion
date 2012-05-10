deac2a6cfc14a1c0bdd228e601368eb3d515e71b
Francis Chong <francis@ignition.hk>
2012-05-10 11:43:34 +0800

    fix number conversion; refactor

--- app/supports/number_ext.rb
+++ app/supports/number_ext.rb
@@ -3,11 +3,11 @@ PI_HALF  = 1.57079632679
 TWO_PI   = 6.28318530718
 
 module GameNumbers
-  def to_rad
+  def to_degree
     ((self) / PI * 180.0)
   end
   
-  def to_degree
+  def to_rad
     ((self) / 180.0 * PI)
   end
 end
